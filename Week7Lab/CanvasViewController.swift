8aac94e7dd52789f5c4c5015107aeca8380597c0
Honorio Vega <hvega@csumb.edu>
2018-03-12 15:13:14 -0700

    3. The tray will continue to the up or down position based on the direction the user was last panning

--- Week7Lab/CanvasViewController.swift
+++ Week7Lab/CanvasViewController.swift
@@ -14,12 +14,21 @@ class CanvasViewController: UIViewController {
     
     // Create a property in your view controller to store the coordinates of the tray's original position
     var trayOriginalCenter: CGPoint!
-
+    
+    //Create two view controller properties to store the tray's position when it's "up" and "down" as well as the offset amount that the tray will move down.
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        trayDownOffset = 220
+        trayUp = trayView.center // The initial position of the tray
+        
+        // The position of the tray transposed down
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,7 +40,7 @@ class CanvasViewController: UIViewController {
         let translation = sender.translation(in: view)
 
 //        let location = sender.location(in: view)
-//        let velocity = sender.velocity(in: view)
+            let velocity = sender.velocity(in: view)
 //        let translation = sender.translation(in: view)
 //
         if sender.state == .began {
@@ -44,6 +53,18 @@ class CanvasViewController: UIViewController {
             
         } else if sender.state == .ended {
             print("Gesture ended")
+            if(velocity.y > 0) {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                }, completion: nil)
+                
+            } else {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                }, completion: nil)
+            }
         }
     }
     
