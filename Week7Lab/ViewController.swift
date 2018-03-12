849a95d06ca626efba5a06bf397a3de27b121203
Honorio Vega <hvega@csumb.edu>
2018-03-12 14:09:26 -0700

    Initial Commit

---
+++ Week7Lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week7Lab
+//
+//  Created by Honorio Vega on 3/12/18.
+//  Copyright © 2018 Honorio Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
